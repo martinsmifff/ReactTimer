8d018566871f0e9f017cb007dba7f6163e3548b0
smithm <martin.smith@dowjones.com>
2017-05-27 08:33:06 +0100

    Add navigation

---
+++ app/components/Timer.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var Timer = React.createClass({
+  render: function() {
+    return (
+    <div>
+      Timer.jsx
+    </div>
+    );
+  }
+});
+module.exports = Timer;
