cb7f646bbf5a1d8bf5ca02bb98a3dae10ad5aef0
smithm <martin.smith@dowjones.com>
2017-05-27 15:20:53 +0100

    add title and more tests

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -38,7 +38,36 @@ describe('Countdown', () => {
       }, 3010);
     });
 
+    it('should pause countdown on paused status', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown />);
+      countdown.handleSetCountdown(3);
+      
+      countdown.handleStatusChange('paused');
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(3);
+        expect(countdown.state.countdownStatus).toBe('paused');
+        done();
+      }, 1010);
+
+    });
+
+    it('should reset count when stopped', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown />);
+      countdown.handleSetCountdown(3);
+      
+      countdown.handleStatusChange('stopped');
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(0);
+        expect(countdown.state.countdownStatus).toBe('stopped');
+        done();
+      }, 1010);
+
+    });
   });
+
+
 
 
 });
